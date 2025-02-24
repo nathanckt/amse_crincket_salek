4e083072cc7df48fada8beabb87b657c6ba8c1c5
Nathan Crincket <crincketnathan@gmail.com>
2025-02-24 17:09:01 +0100

    exo4

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'exo1.dart'; 
 import 'exo2.dart';
+import 'exo4.dart';
 
 
 void main() {
@@ -13,8 +14,57 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'TP Flutter',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: Exo2(),
+      home: MenuPage(),
     );
   }
 }
 
+
+class MenuPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Menu')),
+      body: ListView(
+        children: [
+          Card(
+            elevation: 5,
+            child: ListTile(
+              title: Text('Exercice 1'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Exo1()),
+                );
+              },
+            ),
+          ),
+          Card(
+            elevation: 5,
+            child: ListTile(
+              title: Text('Exercice 2'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Exo2()),
+                );
+              },
+            ),
+          ),
+          Card(
+            elevation: 5,
+            child: ListTile(
+              title: Text('Exercice 4'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => DisplayTileWidget()),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
